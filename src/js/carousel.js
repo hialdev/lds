794c6e83739bbff6518cadde9752d6bdda1a68c2
hialdev <al.developer12@gmail.com>
2023-01-23 19:35:38 +0700

    first commit

---
+++ src/js/carousel.js
@@ -0,0 +1,20 @@
+$('.brand-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:3,
+        },
+        600:{
+            items:4,
+        },
+        1000:{
+            items:5,
+        }
+    }
+})
